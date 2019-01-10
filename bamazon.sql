b9797dd216fdd5e57f5df9d4a06baf9a8e6a256f
min <mjnorthwesternwork1121@gmail.com>
2019-01-09 22:40:29 -0600

    added datebase and working custome cli inerface

--- bamazon.sql
+++ bamazon.sql
@@ -1,13 +1,13 @@
-DROP DATABASE IF EXISTS bamazon;
-CREATE DATABASE bamazon;
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
 
-USE bamazon;
+USE bamazon_db;
 
 CREATE TABLE products(
-  id INT NOT NULL AUTO_INCREMENT,
-  item_name VARCHAR(100) NOT NULL,
-  category VARCHAR(45) NOT NULL,
-  starting_bid INT default 0,
-  highest_bid INT default 0,
-  PRIMARY KEY (id)
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price INT default 0,
+  stock_quantity INT default 0,
+  PRIMARY KEY (item_id)
 );
---
+++ bamazonCustomer.js
@@ -0,0 +1,70 @@
+// Dependencies
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+// Connect to database
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'min95595',
+  database : 'bamazon_db'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
+// Show ids, names, and prices of products
+connection.query('SELECT * FROM `products`', function (err, results, fields) {
+  if (err) {
+    console.log(err);
+  }
+  for (var i=0; i<results.length; i++) {
+    console.log(results[i].item_id + " " + results[i].product_name + " [" + results[i].price + "]");
+  }
+  // Prompt user to select a product and enter desired quantity
+  function buyPrompt() {
+    inquirer.prompt( [{
+      name: "itemId",
+      type: "input",
+      message: "Enter the id of the product you'd like to buy."
+    }, {
+      name: "quantity",
+      type: "input",
+      message: "How many units would you like to purchase?"
+    }]).then(function(answer) {
+      for (var i=0; i<results.length; i++) {
+        if (results[i].item_id === parseInt(answer.itemId)) {
+          // If order quantity is too high, notify user of insufficient stock
+          if (results[i].stock_quantity < parseInt(answer.quantity)) {
+            console.log("Insufficient stock!");
+            buyPrompt();
+          } else {
+            // Calculate order total and remaining stock
+            let total = parseFloat(answer.quantity*results[i].price).toFixed(2);
+            let newStock = results[i].stock_quantity - answer.quantity;
+
+            // Construct query to update stock
+            const updateStock = 'UPDATE `products` SET `stock_quantity` = ' + newStock + ' WHERE `item_id` = ' + answer.itemId
+            connection.query(updateStock, function(err, result) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log(result.affectedRows + " product updated");
+              }
+            });
+
+            // Notify user of successful purchase
+            console.log("You have purchased " + answer.quantity + " " + results[i].product_name);
+            console.log("Your order total is " + total);
+          }
+        }
+      }
+    });
+  }
+  buyPrompt();
+});
